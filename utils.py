e11d89c02194c3284a10e643431e6297f160f4b9
Vuong Pham <vuong.pham@theqs.io>
2018-03-22 13:00:09 +0700

    add show_plot func

--- utils.py
+++ utils.py
@@ -1,5 +1,9 @@
 import math
 import time
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+import numpy as np
+
 def as_minutes(seconds):
     minutes = math.floor(seconds / 60)
     seconds -= minutes * 60
@@ -11,3 +15,10 @@ def time_since(since, percent):
     elapsed_seconds = seconds / (percent)
     rest_seconds = elapsed_seconds - seconds
     return '%s (- %s)' % (as_minutes(seconds), as_minutes(rest_seconds))
+
+def show_plot(points):
+    plt.figure()
+    fig, ax = plt.subplots()
+    loc = ticker.MultipleLocator(base=0.2) # put ticks as regular intervals
+    ax.yaxis.set_major_locator(loc)
+    plt.plot(points)
